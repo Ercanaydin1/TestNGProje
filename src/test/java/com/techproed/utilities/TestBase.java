d41d509a7e9872156080cca3c6c464fc7b21f784
ercnaydin1 <ercnaydin1@gmail.com>
2020-09-08 22:16:59 +0200

    ders sonu 16

--- src/test/java/com/techproed/utilities/TestBase.java
+++ src/test/java/com/techproed/utilities/TestBase.java
@@ -6,25 +6,20 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import java.util.concurrent.TimeUnit;
 public abstract class TestBase {
-
     // private -> sadece aynı class
     // default -> aynı pakette
     // protected -> aynı paket + child class'larda
     // public -> heryerde
     protected WebDriver driver;
-
     @BeforeClass
-    public void setUp() {
+    public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
-
     @AfterClass
-    public void tearDown() {
+    public void tearDown(){
         //driver.quit();
     }
 }
-
-
